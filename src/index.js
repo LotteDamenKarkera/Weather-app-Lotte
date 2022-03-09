daa6848c11ddeabdf9b09fab013d3d4987a7a43d
Lotte Damen-Karkera <lottedamen1994@gmail.com>
2022-03-09 12:29:18 +0100

    Update HTML & CSS completely

--- src/index.js
+++ src/index.js
@@ -1,59 +1,54 @@
-function formatDate(timestamp) {
-    let date = new Date(timestamp);
-    let hours = date.getHours();
-    if (hours < 10) {
-        hours = `0${hours}`;
-    }
-    let minutes = date.getMinutes();
-    if (minutes < 10) {
-        minutes = `0${minutes}`;
-    }
-    let days = [
-        "Sunday",
-        "Monday",
-        "Tuesday",
-        "Wednesday",
-        "Thursday",
-        "Friday",
-        "Saturday"
-      ]; 
-      let day = days[date.getDay()];
-      return `${day} ${hours}:${minutes}`;
+let now = new Date();
+
+let weekDays = document.querySelector("#weekDay");
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
+let day = days[now.getDay()];
+weekDays.innerHTML = day;
+
+let hours = now.getHours();
+if (hours < 10) {
+  hours = "0" + hours;
 }
+
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = "0" + minutes;
+}
+
+let time = document.querySelector("#currentTime");
+time.innerHTML = `${hours}:${minutes}`;
 
 //
 
+let searchText = document.querySelector("#changeyourcity");
+let changeCity = document.querySelector("#changeCity");
+changeCity.addEventListener("submit", searchCity);
+
 function currentTemperature(response) {
-    let temperatureElement = document.queryselector("#temperature");
-    let cityElement = document.queryselector("#city");
-    let descriptionElement = document.queryselector("#description");
-    let humidityElement = document.queryselector("#humidity");
-    let windElement = document.queryselector("#speed");
-    let dateElement= document.querySelector("#date");
-    let iconElement = document.querySelector("#icon");
-    temperatureElement.innerHTML = Math.round (response.data.main.temp);
-    cityElement.innerHTML = response.data.name;
-    descriptionElement.innerHTML = response.data.weather[0].description;
-    humidityElement.innerHTML = response.data.main.humidity;
-    windElement.innerHTML = Math.round(response.data.wind.speed);
-    dateElement.innerHTML = formatDate(response.data.dt * 1000);
-    iconElement.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
-    iconElement.setAttribute("alt", response.data.weather[0].description);
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#current-temp").innerHTML = Math.round(
+    response.data.main.temp);
+  document.querySelector("#description").innerHTML = response.data.weather[0].description;
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#speed").innerHTML = Math.round(response.data.wind.speed);
+  document.querySelector("#icon").innerHTML = iconElement.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-function search(city) {
-    let apiKey = "7000e87df583f2a2518b94b8d5460599";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(currentTemperature);
+function searchCity(event) {
+  event.preventDefault(event);
+  let searchTextValue = searchText.value;
+  let city = document.querySelector("#city");
+  city.innerHTML = searchTextValue;
+  let apiKey = "7000e87df583f2a2518b94b8d5460599";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchTextValue}&units=metric&appid=${apiKey}`;
+  axios.get(apiUrl).then(currentTemperature);
 }
-
-function handleSubmit(event) {
-    event.preventDefault();
-    let cityInputElement = document.querySelector("#city-Input");
-    search(cityInputElement.value);
-}
-
-let form = document.queryselector("#changeCity");
-form.addEventListener("submit", handleSubmit);
-
-search("Mumbai");
