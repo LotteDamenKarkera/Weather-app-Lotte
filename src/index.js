120b105092a3415397aaf3217b6ed3df69429c29
Lotte Damen-Karkera <lottedamen1994@gmail.com>
2022-03-22 19:50:29 +0100

    Update function displayForecast

--- src/index.js
+++ src/index.js
@@ -32,7 +32,8 @@ let searchText = document.querySelector("#changeyourcity");
 let changeCity = document.querySelector("#changeCity");
 changeCity.addEventListener("submit", searchCity);
 
-function displayForecast(){
+function displayForecast(response){
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("forecast");
   let forecastHTML = `<div class ="row">`;
   let days = ["Thu", "Fri", "Sat", "Sun"];
@@ -76,6 +77,7 @@ function getForecast(coordinates){
   console.log(coordinates);
   let apiKey = "7000e87df583f2a2518b94b8d5460599";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function currentTemperature(response) {
@@ -105,5 +107,3 @@ function searchCity(event) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchTextValue}&units=metric&appid=${apiKey}`;
   axios.get(apiUrl).then(currentTemperature);
 }
-
-displayForecast();
