1969dc469c9458b099a3f71f05d6c2e6d2314466
Lotte Damen-Karkera <lottedamen1994@gmail.com>
2022-03-22 19:45:29 +0100

    Add apiUrl forecast

--- src/index.js
+++ src/index.js
@@ -73,7 +73,9 @@ function displayForecast(){
 }
 
 function getForecast(coordinates){
-  console.log(coordinates)
+  console.log(coordinates);
+  let apiKey = "7000e87df583f2a2518b94b8d5460599";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
 }
 
 function currentTemperature(response) {
