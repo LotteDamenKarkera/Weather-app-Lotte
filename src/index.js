abae8d0e36bf922a3531d815866ff98dbe57455d
Lotte Damen-Karkera <lottedamen1994@gmail.com>
2022-03-23 14:59:32 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -46,7 +46,7 @@ function displayForecast(response) {
 
   let forecastHTML = `<div class ="row">`;
   forecast.forEach(function(forecastDay, index) {
-    if (index < 6) {
+    if (index < 5) {
 
     forecastHTML = forecastHTML + `
     <div class="col-5">
